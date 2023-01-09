963f2f0e416ac1c1baa0e489f6fc07371c177a6a
chubinashvili <chubinashvili@pop-os.localdomain>
2023-01-09 15:27:56 +0400

    feat: added staking page

--- src/components/Staking/Staking.js
+++ src/components/Staking/Staking.js
@@ -1,16 +1,142 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-import { Button } from "@cubitrix/cubitrix-react-ui-module";
+// import { Button } from "@cubitrix/cubitrix-react-ui-module";
+import './Staking.css';
+import '../../assets/css/main-theme.css';
+import InfoCircle from '../../assets/svgs/info-circle.svg';
+import CurrentStakeIcon from '../../assets/svgs/current-stake.svg';
+import EarnIcon from '../../assets/svgs/earn-icon.svg';
+import RewardIcon from '../../assets/svgs/reward-icon.svg';
+import WalletIcon from '../../assets/svgs/wallet-money-icon.svg';
+import TotalStackedIcon from '../../assets/svgs/total-stacked-icon.svg';
+import TotalUnStakedIcon from '../../assets/svgs/total-unstaked-icon.svg';
+
+const stakeFakeData = [
+  {
+      id: 1,
+      amount: '1,220,000.2',
+      stakeDate: '01.02.2023 10:00AM',
+      unstakeDate: '01.02.2023 08:15PM',
+      reward: 'CML',
+      harvest: '1,132,000.1',
+  }, {
+    id: 2,
+    amount: '1,220,000.2',
+    stakeDate: '01.02.2023 10:00AM',
+    unstakeDate: '01.02.2023 08:15PM',
+    reward: 'CML',
+    harvest: '1,132,000.1',
+  }, {
+    id: 3,
+    amount: '1,220,000,000.2',
+    stakeDate: '01.02.2023 10:00AM',
+    unstakeDate: '01.02.2023 08:15PM',
+    reward: 'CML',
+    harvest: '1,132,000.1',
+  }, 
+]
 
 export const Staking = (props) => {
   return (
-    <div>
-      <Button
-        label={"Trade Button"}
-        size={"btn-lg"}
-        type={"btn-primary"}
-        arrow={"arrow-right"}
-      />
+    <div className='staking-container'>
+      <div className="staking-left">
+        <div className="staking-left__info-container">
+          <div className="staking-left__info">
+            <h2 className="font-16">Staked</h2>
+            <p className="font-14">Bidding Balance <span>0</span></p>
+            <p className="font-14">Bidding Stakers <span>0</span></p>
+          </div>
+        </div>
+        <h2 className="font-14 staking-left__header">Staking Calculator</h2>
+        <p className="staking-left__input">
+          <label>Amount</label>
+          <input type='text' placeholder="0.0000"/>
+          <span className="font-12">MAX</span>
+        </p>
+        <ul className="staking-left__list">
+          <li className="active">30 D</li>
+          <li>60 D</li>
+          <li>90 D</li>
+          <li>100 D</li>
+          <li>360 D</li>
+        </ul>
+        <div className="staking-left__help">
+          <img src={InfoCircle} />
+          <p className="font-10">15 % APY On 30 Days. Locked Until 02/02/2023 2:33 PM</p>
+        </div>
+        <button className="staking-left__btn">Stake</button>
+      </div>
+      <div className="staking-right">
+        <h2 className="staking-right__header">Your Stake</h2>
+        <div className="staking-right__info">
+          <div className="staking-right__info-item">
+              <div className="staking-right__info-element font-16">
+                  <p>
+                    <img src={CurrentStakeIcon} />
+                    Current Stake
+                  </p>
+                  <p>1,220.2 CML</p>
+              </div>
+              <div className="staking-right__info-element">
+                  <p className="font-16">
+                    <img src={EarnIcon} />
+                    Earn
+                  </p>
+                  <p >1,020 CML</p>
+              </div>
+              <div className="staking-right__info-element">
+                  <p>
+                    <img src={RewardIcon} />
+                    Claimed Reward
+                  </p>
+                  <p>1,132.1 CML</p>
+              </div>
+          </div>
+          <div className="staking-right__info-item">
+              <div className="staking-right__info-element font-16">
+                  <p>
+                    <img src={WalletIcon} />
+                    Your Wallet Balance
+                  </p>
+                  <p>1,220.2 CML</p>
+              </div>
+              <div className="staking-right__info-element">
+                  <p className="font-16">
+                    <img src={TotalStackedIcon} />
+                    Total Stacked
+                  </p>
+                  <p >1,020 CML</p>
+              </div>
+              <div className="staking-right__info-element">
+                  <p>
+                    <img src={TotalUnStakedIcon} />
+                    Total Unstaked
+                  </p>
+                  <p>1,132.1 CML</p>
+              </div>
+          </div>
+        </div>
+        <table className="staking-right__stakes">
+              <tr className="staking-right__stakes-head font-14">
+                <th>Staked Amount</th>
+                <th>Stake Date</th>
+                <th>Unstake Date</th>
+                <th>Earm Reward</th>
+                <th>Harvest</th>
+              </tr>
+             {stakeFakeData.map(item => (
+                <tr className="staking-right__stakes-content">
+                  <td>{item.amount}</td>
+                  <td>{item.stakeDate}</td>
+                  <td>{item.unstakeDate}</td>
+                  <td>{item.reward}</td>
+                  <td>{item.harvest}</td>
+                  <button className="unstake-btn font-14">Unstake</button>
+                  <button className="harvest-btn font-14">Harvest</button>
+                </tr>
+              ))}
+          </table>
+      </div>
     </div>
   );
 };
